ac9f80b8bc095afe8688a87a03265371b1fff292
chiy4728 <chiy4728@gmail.com>
2025-01-24 14:49:12 +0800

    Update and rename extract_vless_links.py to extract_links.py

--- extract_links.py
+++ extract_links.py
@@ -13,7 +13,7 @@ def extract_vless_vmess_links(url):
             r'vless://[^\s<>"]+\?[^\s<>"]+#?[^\s<>"]*', content
         )
         vmess_links = re.findall(
-            r'vmess://[^\s<>"]+\?[^\s<>"]+#?[^\s<>"]*', content
+            r'vmess://[^\s<>"]+', content
         )
 
         if not vless_links and not vmess_links:
@@ -24,11 +24,23 @@ def extract_vless_vmess_links(url):
         decoded_vmess_links = [html.unescape(link) for link in vmess_links]
 
         # 去重链接
-        unique_links = set(decoded_vless_links + decoded_vmess_links)
+        unique_vless_links = set(decoded_vless_links)
+        unique_vmess_links = set(decoded_vmess_links)
+        unique_links = list(unique_vless_links.union(unique_vmess_links))
+
+        print("\n提取的 VLESS 链接:")
+        for link in unique_vless_links:
+            print(link)
+        print("\n提取的 VMESS 链接:")
+        for link in unique_vmess_links:
+            print(link)
         if len(unique_links) < (len(decoded_vless_links) + len(decoded_vmess_links)):
             print("Duplicate links found and removed.")
 
         combined_links = list(unique_links) # 将去重后的链接列表化
+        print("\n去重后的链接:")
+        for link in combined_links:
+            print(link)
 
         vless_vmess_combined = "\n".join(combined_links)
         subscription_link = base64.urlsafe_b64encode(vless_vmess_combined.encode()).decode()
